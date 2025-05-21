d1ce0df8bc4ac457ec014e4e68a7cda0da7829c7
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-05-21 11:52:37 -0300

    [BAK - 0012]: Gerado classes do EmployeeData

---
+++ src/main/java/io/unodevs/SoBarba/model/EmployeeData.java
@@ -0,0 +1,73 @@
+package io.unodevs.SoBarba.model;
+
+import io.unodevs.SoBarba.model.enumerator.JobTitleType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class EmployeeData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate hireDate;
+    private LocalDate terminationDate;
+    private Double salary;
+    private Double commission;
+    private JobTitleType jobTitle;
+
+    public EmployeeData() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public LocalDate getTerminationDate() {
+        return terminationDate;
+    }
+
+    public void setTerminationDate(LocalDate terminationDate) {
+        this.terminationDate = terminationDate;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(Double commission) {
+        this.commission = commission;
+    }
+
+    public JobTitleType getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(JobTitleType jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
--- src/main/java/io/unodevs/SoBarba/model/enumerator/JobTitleType.java
+++ src/main/java/io/unodevs/SoBarba/model/enumerator/JobTitleType.java
@@ -1,4 +1,11 @@
 package io.unodevs.SoBarba.model.enumerator;
 
 public enum JobTitleType {
+    BARBER,
+    RECEPTIONIST,
+    MANAGER,
+    ASSISTANT,
+    CASHIER,
+    CLEANING_STAFF,
+    TRAINEE
 }

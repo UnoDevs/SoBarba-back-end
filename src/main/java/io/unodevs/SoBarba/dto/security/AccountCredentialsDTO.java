584d764e3668dbbf4b95eb2fd1596ca89e82b229
Veronica Santos <felipe.santosmf2022@gmail.com>
2025-05-25 11:51:53 -0300

    [BAK - 0014]: Implementado DTO de Token e Credenciais e JWTProvider e Filter

---
+++ src/main/java/io/unodevs/SoBarba/dto/security/AccountCredentialsDTO.java
@@ -0,0 +1,30 @@
+package io.unodevs.SoBarba.dto.security;
+
+import java.io.Serializable;
+
+public class AccountCredentialsDTO implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    private String username;
+    private String password;
+
+    public AccountCredentialsDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/io/unodevs/SoBarba/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,32 @@
+package io.unodevs.SoBarba.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filter) throws IOException, ServletException {
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
+        if (StringUtils.isNotBlank(token) && tokenProvider.validateToken(token)){
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            if (authentication != null){
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filter.doFilter(request,response);
+    }
+}
